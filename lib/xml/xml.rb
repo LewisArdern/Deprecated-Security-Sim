2e0c5010f0fcdeadb920a6833485ca5709ed7955
Lewis Ardern <lewisardern@Lewiss-MacBook-Pro.local>
2014-03-05 22:05:48 +0000

    xml now reading through nokogiri

--- lib/xml/xml.rb
+++ lib/xml/xml.rb
@@ -1,5 +1,26 @@
 require 'nokogiri'
 
+ 
+
 doc = Nokogiri::XML(File.open("boxesconfig.xml"))
-@block = doc.css("Systems").map {|node| node.children.text}
-puts @block
+
+
+doc.search('//systems/system').each do |system|
+    systemNumber  = system.at('@number').text
+    os  = system.at('//os/@system').text
+    base = system.at('//os/@basebox').text
+   
+    #vulnerabilities << { "critical" => system.at('//vulnerability/@critical').text, "access" => system.at('//vulnerability/@access').text}
+  	  vulnerabilities = system.css('vulnerabilities vulnerability').collect do |vulnerability| 
+ 	  { 'critical' => vulnerability['critical'], 'access' => vulnerability['access'] } 
+	end
+	  	  networks = system.css('networks network').collect do |network| 
+ 	  { 'network' => network['name'] } 
+	end
+
+    puts vulnerabilities
+    puts systemNumber
+    puts os
+    puts networks    
+end
+
